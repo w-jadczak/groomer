718fbb244f00723511ab428edb684a84cc852244
jadczakwo <wojciech.jadczak@trumpf.com>
2025-02-02 22:24:18 +0100

    adding serializer and basic view for users app

---
+++ backend/pets/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from users.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'mobile', 'role', 'is_active', 'created_at', 'updated_at')
+        read_only_fields = ('id', 'created_at', 'updated_at')
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
