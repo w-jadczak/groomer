bbdc8f43e75231255c1dde4df03a2827ee2e560f
jadczakwo <wojciech.jadczak@trumpf.com>
2025-02-19 21:12:10 +0100

    fixing auth module rename bug

--- backend/user_security/urls.py
+++ backend/user_security/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 
-from user_security import views
 from user_security.views import RegisterView, CustomTokenObtainPairView
 
 app_name = "user_security"
--- backend/user_security/views.py
+++ backend/user_security/views.py
@@ -1,8 +1,12 @@
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.status import HTTP_201_CREATED
+from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 from user_security.serializers import CustomTokenObtainPairSerializer, RegisterSerializer
+from users.models import User
 
 
 class CustomTokenObtainPairView(TokenObtainPairView):
@@ -10,5 +14,24 @@ class CustomTokenObtainPairView(TokenObtainPairView):
 
 
 class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = RegisterSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+
+        refresh = RefreshToken.for_user(user)
+
+        from rest_framework import status
+
+        return Response(
+            {
+                "user": serializer.data,
+                "refresh": str(refresh),
+                "access": str(refresh.token),
+            },
+            status=status.HTTP_201_CREATED,
+        )
