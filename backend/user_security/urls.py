1a3ff3831b571bff1a553cdbe53b908e8c32cecb
jadczakwo <wojciech.jadczak@trumpf.com>
2025-03-02 22:38:33 +0100

    rebuilding login view to set token as cookie

--- backend/user_security/urls.py
+++ backend/user_security/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 
-from user_security.views import RegisterView, CustomTokenObtainPairView, LogoutView
+from user_security.views import RegisterView, LoginView, LogoutView
 
 app_name = "user_security"
 
 urlpatterns = [
-    path("token/", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("login/", LoginView.as_view(), name="login"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("register/", RegisterView.as_view(), name="register"),
     path("logout/", LogoutView.as_view(), name="logout"),
