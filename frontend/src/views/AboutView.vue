fc7746f409bbb88d0b8ff0d61ddb45e4b0d0ff23
jadczakwo <wojciech.jadczak@trumpf.com>
2025-03-25 16:32:17 +0100

    rebuilding frontend

---
+++ frontend/src/views/AboutView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <div class="bg-blue-500 text-red p-6 font-semibold py-10">Tailwind działa poprawnie!</div>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
+<script setup lang="ts"></script>
