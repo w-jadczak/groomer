a06980948d345450876acb42144c82e6d25f67d0
BartlomiejPias <68026725+BartlomiejPias@users.noreply.github.com>
2025-05-04 19:50:44 +0200

    Feed/pets endpoint (#3)
    * test commit
    
    * wyrównanie do wersji z develop, zrównanie folderów, dodanie requirements
    
    * usunięcie folderu groomer
    
    * Pets serializer, views (CRUD), urls
    
    * commit z CRUD pets
    
    * commit CRUD
    
    * pulling from develop
    
    ---------
    
    Co-authored-by: Bartlomiej Piasecki <tuuunis3@gmail.com>

--- backend/groomer/urls.py
+++ backend/groomer/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/users/", include("users.urls")),
     path("api/auth/", include("user_security.urls")),
+    path("api/pets/", include("pets.urls")),
 ]
